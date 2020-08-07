298e03f31eca151924b8cdc2232379b2b7a06428
vc_tamir <vc.tamir@gmail.com>
2020-08-07 15:50:39 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,61 @@
+<h2 align="center">
+Proffy
+</h2>
+
+<p align="center"> 
+<img src="https://uploaddeimagens.com.br/images/002/810/003/full/Logo.png?1596480458" width="300" heigth="300">
+</p>
+
+<p align="center">
+  <a href="LICENSE" >
+<img alt="License" src="https://img.shields.io/badge/license-MIT-%23F8952D">
+  </a>
+</p>
+
+<h3 align="center">  
+  <a href="#information_source-sobre">Sobre</a> |
+  <a href="#interrobang-motivo">Motivo</a> | 
+  <a href="#rocket-tecnologias-utilizadas">Tecnologias</a> | 
+  <a href="#link-como-contribuir">Como Contribuir</a> | 
+  <a href="#licença">Licença</a> 
+</h3>
+
+<img src="https://uploaddeimagens.com.br/images/002/810/002/original/Anota%C3%A7%C3%A3o_2020-08-03_154536.png?1596480365" width="1200">
+<p align="center">
+<img align="right" src="https://uploaddeimagens.com.br/images/002/817/708/full/Mobile.png?1596824978">
+</p>
+
+## :information_source: Sobre
+
+Plataforma de estudos online, com objetivo de conectar alunos e professores para aulas on-line.
+
+## :interrobang: Motivo
+
+Desenvolvimento do projeto Next Level Week, desenvolvido pela Rocketseat.
+
+## :rocket: Tecnologias Utilizadas
+
+O projeto foi desenvolvido utilizando as seguintes tecnologias
+
+- Typescript
+- ReactJS
+- ReactNative
+- Expo
+- Css
+- Html
+- NodeJS
+- Express
+- Knex
+- Yarn
+
+## :link: Como contribuir
+
+- Faça um Fork do repositório,
+- Faça um clone do respositório
+- Crie uma branch com a sua feature
+- Faça um commit com suas mudanças
+- Push a sua branch
+- Ir em Pull Requests do projeto original e criar uma pull request com o seu commit
+
+## Licença
+Esse projeto está sob a licença MIT. Veja o arquivo [LICENSE](LICENSE) para mais detalhes.
---
+++ web/src/pages/TeacherList/index.tsx
@@ -0,0 +1,84 @@
+import React, { useState, FormEvent } from 'react';
+
+import './styles.css';
+
+import PageHeader from '../../components/PageHeader';
+import TeacherItem, { Teacher } from '../../components/TeacherItem';
+import Input from '../../components/Input';
+import Select from '../../components/Select';
+import api from '../../services/api';
+
+function TeacherList() {
+    const [subject, setSubject] = useState('');
+    const [week_day, setWeek_day] = useState('');
+    const [time, setTime] = useState('');
+    const [teachers, setTeachers] = useState([]);
+
+    async function searchTeachers(e: FormEvent){
+        e.preventDefault();
+
+        const response = await api.get('classes', {
+            params: {
+                subject,
+                week_day,
+                time
+            }
+        });
+
+        setTeachers(response.data);
+    }
+
+    return (
+        <div id="page-teacher-list" className="container">
+            <PageHeader title="Estes são os proffys disponíveis">
+                <form id="search-teachers" onSubmit={searchTeachers}>
+                    <Select
+                        label="Matéria"
+                        name="subject"
+                        value={subject}
+                        options={[
+                            { value: 'Artes', label: 'Artes' },
+                            { value: 'Matemática', label: 'Matemática' },
+                            { value: 'Ciências', label: 'Ciências' },
+                            { value: 'Geografia', label: 'Geografia' },
+                            { value: 'Português', label: 'Português' },
+                            { value: 'História', label: 'História' }
+                        ]}
+                        onChange={e => setSubject(e.target.value)}
+                    />
+                    <Select
+                        label="Dia da semana"
+                        name="weekday"
+                        value={week_day}
+                        options={[
+                            { value: '0', label: 'Domingo' },
+                            { value: '1', label: 'Segunda-feira' },
+                            { value: '2', label: 'Terça-feira' },
+                            { value: '3', label: 'Quarta-feira' },
+                            { value: '4', label: 'Quinta-feira' },
+                            { value: '5', label: 'Sexta-feira' },
+                            { value: '6', label: 'Sábado' }
+                        ]}
+                        onChange={e => setWeek_day(e.target.value)}
+                    />
+                    <Input label="Hora" name="time" type="time"
+                        value={time}
+                        onChange={e => setTime(e.target.value)} />
+                    <button type='submit'>
+                        Buscar
+                    </button>
+                </form>
+            </PageHeader>
+
+            <main>
+                {teachers.map((teacher: Teacher) => {
+                    return (
+                        <TeacherItem key={teacher.id} teacher={teacher} />
+                    )
+                })}
+            </main>
+        </div>
+    )
+}
+
+export default TeacherList;
